de49b36c68f0f0b6e31b85dbdd002b53945d79f2
Carlos Monroy Fernández <cmonroy@boardfy.com>
2019-04-23 19:50:02 +0200

    Added xdebug installation

--- php/ubuntu-php.sh
+++ php/ubuntu-php.sh
@@ -13,13 +13,18 @@ fi
 function main {
 	bold=$(tput bold)
 	normal=$(tput sgr0)
-	VERSION="$(echo "$0" | sed 's/\(.\/\)\?\(ubuntu-php\|.sh\)//g')"
+	if [ -z "$PHP_VERSION"] || [ -z "$XDEBUG_VERSION"]; then
+		PHP_VERSION="7.2.17"
+		XDEBUG_VERSION="2.7.1"
+	fi
 	TMP_DIR=/tmp/phpzts
 	INSTALL_DIR=/etc/phpzts
 
+	/bin/rm -rf "$INSTAL_DIR"
 	if [ -d $TMP_DIR ]; then
 		bold_echo "Removing old PHP ZTS data in $TMP_DIR"
 		/bin/rm -rf "$TMP_DIR/*"
+
 	else
 		bold_echo "Creating temporary directory in $TMP_DIR"
 		mkdir "$TMP_DIR"
@@ -34,15 +39,15 @@ function main {
 	libmysqlclient-dev libcurl4-openssl-dev
 
 	bold_echo "Downloading php source"
-	wget https://github.com/php/php-src/archive/php-$VERSION.tar.gz
-	tar --extract --gzip --file php-$VERSION.tar.gz
+	wget https://github.com/php/php-src/archive/php-$PHP_VERSION.tar.gz
+	tar --extract --gzip --file php-$PHP_VERSION.tar.gz
 
 	bold_echo "Downloading Pthreads"
-	cd "$TMP_DIR/php-src-php-$VERSION/ext"
+	cd "$TMP_DIR/php-src-php-$PHP_VERSION/ext"
 	git clone https://github.com/krakjoe/pthreads
 
 	bold_echo "Configuring PHP Build"
-	cd "$TMP_DIR/php-src-php-$VERSION"
+	cd "$TMP_DIR/php-src-php-$PHP_VERSION"
 	./buildconf --force
 
 	CONFIGURE_STRING="
@@ -65,11 +70,11 @@ function main {
 		--enable-debug"
 	./configure $CONFIGURE_STRING
 
-	bold_echo "Making & Installing php $VERSION"
+	bold_echo "Making & Installing php $PHP_VERSION"
 	make && make install
 	
 	bold_echo "Creating php.ini"
-	cd "$TMP_DIR/php-src-php-$VERSION"
+	cd "$TMP_DIR/php-src-php-$PHP_VERSION"
 	mkdir -p "$INSTALL_DIR/cli/"
 	cp "php.ini-production $INSTALL_DIR/cli/php.ini"
 
@@ -78,7 +83,15 @@ function main {
 	bold_echo "Creating link for phpzts command"
 	rm /usr/bin/phpzts
 	ln -s $INSTALL_DIR/bin/php /usr/bin/phpzts
-
+	bold_echo "Instaling XDEBUG"
+	cd "$TMP_DIR"
+	wget "https://github.com/xdebug/xdebug/archive/$XDEBUG_VERSION.tar.gz"
+	tar --extract --gzip --file $XDEBUG_VERSION.tar.gz
+	cd xdebug-$XDEBUG_VERSION
+	$INSTALL_DIR/bin/phpize
+	./configure --enable-xdebug --with-php-config=$INSTALL_DIR/bin/php-config
+	make && make install
+	echo "zend_extension=$(phpzts -i | egrep ^extension_dir | sed -r 's/extension_dir => (\S+).*/\1/g')/xdebug.so" | tee -a "$INSTALL_DIR/cli/php.ini"
 	bold_echo "Cleaning tmp folder"
 	rm -rf $TMP_DIR
 

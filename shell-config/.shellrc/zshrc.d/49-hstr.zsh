bdd19c66458bd842d0c3f018afe34fcad8c41c39
Carlos <22024214+Redominus@users.noreply.github.com>
2022-04-30 19:52:14 +0200

    Some fixes

--- shell-config/.shellrc/zshrc.d/49-hstr.zsh
+++ shell-config/.shellrc/zshrc.d/49-hstr.zsh
@@ -1,7 +1,4 @@
-if whence hh >/dev/null 2>&1
-then
-  alias hh=hstr                    # hh to be alias for hstr
-  export HSTR_CONFIG=hicolor       # get more colors
-  export HISTFILE=~/.zsh_history   # ensure history file visibility
-  bindkey -s "\C-r" "\eqhstr\n"    # bind hstr to Ctrl-r (for Vi mode check doc)
-fi
+alias hh=hstr                    # hh to be alias for hstr
+export HSTR_CONFIG=hicolor       # get more colors
+export HISTFILE=~/.zsh_history   # ensure history file visibility
+bindkey -s "\C-r" "\eqhstr\n"    # bind hstr to Ctrl-r (for Vi mode check doc)
--- shell-config/installer.sh
+++ shell-config/installer.sh
@@ -1,10 +1,9 @@
 echo 'Adding Repos zsh & hstr'
-#hstr
-sudo add-apt-repository ppa:ultradvorka/ppa 
 #Alacritty
-sudo add-apt-repository ppa:mmstick76/alacritty 
+sudo add-apt-repository ppa:aslatter/ppa
 echo 'Installing packages'
-sudo apt-get update && sudo apt-get install -y hstr zsh git alacritty
+sudo apt-get update 
+sudo apt install -y tmux hstr htop zsh git alacritty neovim
 
 echo 'Installing TPM'
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -13,10 +12,10 @@ echo 'Downloading fonts'
 if [ ! -d "~/.local/share/fonts" ] ; then
 	mkdir ~/.local/share/fonts
 fi
-wget -O '~/.local/share/fonts/MesloLGS NF Regular.ttf' 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf'
-wget -O '~/.local/share/fonts/MesloLGS NF Bold.ttf' 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf'
-wget -O '~/.local/share/fonts/MesloLGS NF Italic.ttf' 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf'
-wget -O '~/.local/share/fonts/MesloLGS NF Bold Italic.ttf' 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf'
+wget -O "$HOME/.local/share/fonts/MesloLGS NF Regular.ttf" 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf'
+wget -O "$HOME/.local/share/fonts/MesloLGS NF Bold.ttf" 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf'
+wget -O "$HOME/.local/share/fonts/MesloLGS NF Italic.ttf" 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf'
+wget -O "$HOME/.local/share/fonts/MesloLGS NF Bold Italic.ttf" 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf'
 chmod 664 ~/.local/share/fonts/*
 chmod 644 ~/.local/share/fonts/.uuid
 
@@ -35,19 +34,25 @@ mkdir ~/.config/alacritty
 ln -sf ~/.shellrc/alacritty.yml ~/.config/alacritty/alacritty.yml
 echo 'Creating links to config files'
 ln -sf ~/.shellrc/zshrc ~/.zshrc
-ln -sf ~/.shellrc/.p10k.zsh ~/.p10k.zsha
+ln -sf ~/.shellrc/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/.shellrc/.tmux.conf ~/.tmux.conf
+#Reload tmux config to download plugins
+tmux source ~/.tmux.conf
+#default zsh a sshell
+echo default zsh a sshell
+chsh -s $(which zsh)
+
 echo 'Installing docker'
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 echo 'Installing docker compose'
 mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 echo 'Installing kubectl'
 snap install kubectl --classic

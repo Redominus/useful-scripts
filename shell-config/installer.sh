0720bab0067908472cdc93cc2e123ca1b8db0329
Carlos <22024214+Redominus@users.noreply.github.com>
2022-05-02 10:10:58 +0200

    Added user to docker group

--- shell-config/installer.sh
+++ shell-config/installer.sh
@@ -50,6 +50,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+newgrp docker
 echo 'Installing docker compose'
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
